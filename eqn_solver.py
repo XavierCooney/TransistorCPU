3283165eb9777387ab0f7965a5d8364603921079
Xavier Cooney <xavier.cooney03@gmail.com>
2021-12-20 12:38:09 +1100

    Improve eqn_solver.py performance
    Rather than using a dict and then build the matrix with lil_matrix
    entry by entry, instead keep track of each (row, col, data) coefficient
    addition, and then build a coo_matrix from that, and then convert
    it to a csc matrix. For some reason directly building the csc matrix
    takes longer. This offers a significant performance boost to the
    simulation. override_variable is removed, as it just complicates matters
    and it turns out it wasn't ever needed.

--- eqn_solver.py
+++ eqn_solver.py
@@ -1,7 +1,11 @@
 # A small module to solve sytems of linear equations for custom sim
 import math
 import typing as typ
-from collections import defaultdict
+import warnings
+
+import numpy as np
+import scipy.sparse  # type: ignore
+import scipy.sparse.linalg  # type: ignore
 
 
 class System:
@@ -9,12 +13,10 @@ class System:
         self.vars: typ.Dict[str, int] = {}
         self.rows: typ.Dict[str, int] = {}
 
-        MatEntryType = typ.Dict[typ.Tuple[int, int], float]
-        # self.mat_entries indexed by (row, col)
-        self.mat_entries: MatEntryType = defaultdict(int)
+        self.row_entries: typ.List[int] = []
+        self.col_entries: typ.List[int] = []
+        self.data_entries: typ.List[float] = []
         self.const_vec: typ.List[float] = []
-
-        self.variable_overrides: typ.Dict[int, float] = {}
 
         self.approximated = False
 
@@ -39,18 +41,23 @@ class System:
         term_var = self.var_index(term_var_name)
         row_var = self.row_index(row_var_name)
 
-        self.mat_entries[(row_var, term_var)] += coeff
+        self.row_entries.append(row_var)
+        self.col_entries.append(term_var)
+        self.data_entries.append(coeff)
 
     def add_constant(self, constant: float, row_var_name: str) -> None:
         row_var = self.row_index(row_var_name)
         self.const_vec[row_var] += constant
 
-    def override_variable(self, var_name: str, value: float) -> None:
-        # TODO: do this better
-        var = self.var_index(var_name)
-        if var in self.variable_overrides:
-            assert self.variable_overrides[var] == value
-        self.variable_overrides[var] = value
+    def make_matrix(self):  # type: ignore
+        num_rows = len(self.rows)
+        num_vars = len(self.vars)
+        assert num_rows == num_vars
+
+        return scipy.sparse.coo_matrix(
+            (self.data_entries, (self.row_entries, self.col_entries)),
+            shape=(num_rows, num_vars)
+        )
 
     def dump_equation(self) -> str:
         lines = [[f'{row_name:>20})'] for row_name in self.rows]
@@ -59,94 +66,81 @@ class System:
         for var_name, var_idx in self.vars.items():
             var_idx_to_name[var_idx] = var_name
 
-        for coord, coeff in self.mat_entries.items():
+        matrix = self.make_matrix().tolil()  # type: ignore
+
+        coords_already_done = set()
+
+        for row, col in zip(self.row_entries, self.col_entries):
+            if (row, col) in coords_already_done:
+                continue
+            coords_already_done.add((row, col))
+
+            coeff = matrix[row, col]
             if math.isclose(coeff, 0):
                 continue
-            if lines[coord[0]][-1][-1] != ')':  # TODO: yuck
-                lines[coord[0]].append('+')
-            lines[coord[0]].append(f'{coeff} * {var_idx_to_name[coord[1]]}')
+
+            if lines[row][-1][-1] != ')':  # TODO: yuck
+                lines[row].append('+')
+
+            lines[row].append(f'{coeff} * {var_idx_to_name[col]}')
 
         for row_id, constant in enumerate(self.const_vec):
             lines[row_id].append(f'= {constant}')
 
         return '\n'.join(map(' '.join, lines)) + '\n'
 
-    def solve(self) -> typ.Dict[str, float]:
-        # Delay import so no issue if sim not invoked
-        import numpy
-        import scipy.sparse  # type: ignore
-        import scipy.sparse.linalg  # type: ignore
-
-        # print(self.vars, self.rows, self.mat_entries)
-
-        num_rows = len(self.rows)
-        num_vars = len(self.vars) - len(self.variable_overrides)
-        assert num_rows == num_vars
-
-        var_renumbering: typ.Dict[int, int] = {}
-        for var_name, old_var_num in self.vars.items():
-            if old_var_num not in self.variable_overrides:
-                var_renumbering[old_var_num] = len(var_renumbering)
-
-        b_vec = numpy.zeros((num_rows,))
-        for index, constant in enumerate(self.const_vec):
-            b_vec[index] = constant
-
-        # TODO: construct the sparse matrix better?
-        matrix = scipy.sparse.lil_matrix((num_rows, num_vars))
-        for coord, coefficient in self.mat_entries.items():
-            row_var, term_var = coord
-            if term_var in self.variable_overrides:
-                actual_val = coefficient * self.variable_overrides[term_var]
-                b_vec[coord[0]] -= actual_val
-            else:
-                matrix[(row_var, var_renumbering[term_var])] = coefficient
-
-        # print(matrix, b_vec, sep='\n')
+    def sparse_matrix_solve(self, A, b):  # type: ignore
+        USE_PYPARDISO = False  # it actually seems to be slower?>>
+        if USE_PYPARDISO:
+            try:
+                import pypardiso  # type: ignore
+                solution = pypardiso.spsolve(A, b)
+                return solution
+            except ImportError:
+                warnings.warn("Can't import pypardiso")
 
         try:
-            solution = scipy.sparse.linalg.spsolve(matrix.tocsr(), b_vec)
+            solution = scipy.sparse.linalg.spsolve(A.tocsc(), b)
         except RuntimeError:
             import traceback
             traceback.print_exc()
             print()
             print('=' * 80)
             print()
-            solution = scipy.sparse.linalg.lsqr(matrix.tocsr(), b_vec)
+            solution = scipy.sparse.linalg.lsqr(A, b)
             self.approximated = True
+            warnings.warn("Approximating sparse matrix solve!")
 
-        final_result = {
-            var_name: solution[var_renumbering[var_index]]
+        return solution
+
+    def solve(self) -> typ.Dict[str, float]:
+        b_vec = np.array(self.const_vec)
+        matrix = self.make_matrix()  # type: ignore
+
+        solution = self.sparse_matrix_solve(
+            matrix, b_vec
+        )  # type: ignore
+
+        return {
+            var_name: solution[var_index]
             for var_name, var_index in self.vars.items()
-            if var_index not in self.variable_overrides
         }
-        final_result.update({
-            var_name: self.variable_overrides[var_index]
-            for var_name, var_index in self.vars.items()
-            if var_index in self.variable_overrides
-        })
-
-        return final_result
 
 
 if __name__ == '__main__':
-    # x = 5, y = -7, z = 2
-    # 2x + 3y +  z = -9   (1)
-    #  x -  y - 3z =  6   (2)
+    # x = 5, y = -7
+    # 2x + 3y = -11   (1)
+    #  x -  y =  12   (2)
 
     system = System()
 
     system.add_term(2, 'x', '1')
     system.add_term(3, 'y', '1')
-    system.add_term(1, 'z', '1')
-    system.add_constant(-9, '1')
+    system.add_constant(-11, '1')
 
     system.add_term(-1, 'y', '2')
     system.add_term(1, 'x', '2')
-    system.add_term(-3, 'z', '2')
-    system.add_constant(6, '2')
-
-    system.override_variable('z', 2)
+    system.add_constant(12, '2')
 
     print(system.dump_equation())
 
@@ -154,8 +148,6 @@ if __name__ == '__main__':
 
     print(result)
 
-    assert len(result) == 3
     assert math.isclose(result['x'], 5)
     assert math.isclose(result['y'], -7)
-    assert math.isclose(result['z'], 2)
     assert not system.approximated
